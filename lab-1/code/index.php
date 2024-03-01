996986cc64f3d2b4c6d1e21e1e6ae15794c091d2
Alex <gumbinin24@icloud.com>
2024-03-01 20:52:49 +0200

    [lab 2] task 1

--- lab-1/code/index.php
+++ lab-1/code/index.php
@@ -1,3 +1,19 @@
 <?php
-# функция, рассказывающая о всех модулях php
-echo phpinfo();
+
+echo "\n";
+echo "Num 1";
+
+/* Imagine a lot of code here */
+$very_bad_unclear_name = "15 chicken wings ";
+
+
+// Write your code here:
+$oder =& $very_bad_unclear_name;
+$oder .= "Why should I apologize for being a monster? Has anyone ever apologized for turning me into one of them?";
+
+
+// Don't change the line below
+echo "\nYour order is: $very_bad_unclear_name.";
+
+
+
