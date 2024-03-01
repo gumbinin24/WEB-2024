e1b7e8d13f827df7b23a886b80f7f385b4b47ee1
Alex <gumbinin24@icloud.com>
2024-03-01 21:26:33 +0200

    [lab 2] task 2

--- lab-1/code/index.php
+++ lab-1/code/index.php
@@ -15,5 +15,26 @@ $oder .= "Why should I apologize for being a monster? Has anyone ever apologized
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
 
+echo "\nNum 2\n";
+
+#time: 19:33
+$minutes = 33;
+echo $minutes;
+
+$tochka = 10.5;
+echo "\n";
+
+echo $tochka;
+echo "\n";
+
+echo $minutes - $tochka - $tochka;
+echo "\n";
+
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+
+echo $last_month - $this_month;
+
 
 
