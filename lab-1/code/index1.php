895cc0772d67d47928b8630da96d7a9bbaf790c4
Alex <gumbinin24@icloud.com>
2024-03-16 19:19:30 +0200

    [lab 3] Task 1 (b) Regular Expressions

--- lab-1/code/index1.php
+++ lab-1/code/index1.php
@@ -8,4 +8,17 @@ echo "Строчки, соответствующие шаблону:\n";
 print_r($matches[0]);
 echo "\n";
 
+echo "Task 1b \n";
+$str2 = 'a1b2c3';
+echo "Строка до преобразования " .$str2 ."\n";
+$patterns2 = [];
+$patterns2[0] = '/1/';
+$patterns2[1] = '/2/';
+$patterns2[2] = '/3/';
+$replacements = [];
+$replacements[0] = '1';
+$replacements[1] = '4';
+$replacements[2] = '9';
+$result2 = preg_replace($patterns2, $replacements, $str2);
+echo "Строка после преобразования " .$result2 ."\n";
 
